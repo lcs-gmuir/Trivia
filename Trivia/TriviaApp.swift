3b6627231f613cac415be396dcae19766ee1cb45
griffin muir <gmuir@lcs.on.ca>
2023-04-20 09:15:42 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Samantha Stewart on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
