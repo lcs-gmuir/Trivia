ce371febebafac1a35770943d5cc9a6f5a7c984f
griffin muir <gmuir@lcs.on.ca>
2023-05-28 20:02:46 -0400

    changed stored properties so they are also declared in the local data base

--- Trivia/Model/Advice.swift
+++ Trivia/Model/Advice.swift
@@ -3,15 +3,15 @@
 //  AdviceList
 //
 //  Created by Samantha Stewart on 2023-04-19.
-//
 
 
+import Blackbird
 import Foundation
 
 
-struct Advice: Identifiable, Codable {
-    let  advice: String
-    let id: Int
+struct Advice: Identifiable, Codable, BlackbirdModel{
+    @BlackbirdColumn var advice: String
+    @BlackbirdColumn var id: Int
 }
 
 let exampleAdvice = Advice(advice: "if your in a cave dont go crazy", id: 2)
